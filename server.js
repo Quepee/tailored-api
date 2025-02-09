a4c1b9bfd5d7d9e3cde7c381f82b31866e7fd300
pratt <xcellersqp@gmail.com>
2025-02-09 17:41:18 +0100

    Added root route for API

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const classifyRoute = require("./src/routes/classifyRoute");
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Number Classification API" });
+});
+
+
 const app = express();
 app.use(cors()); // Handle CORS
 app.use(express.json()); // Parse JSON request bodies
